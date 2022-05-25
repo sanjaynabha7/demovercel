a4e29bd020d68ce89782e5be833981d188f9bd2d
Sanjay Kumar <sanjaynabha7@gmail.com>
2022-05-25 15:04:24 +0530

    add model in mongoo

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,20 +1,8 @@
 import initDB from '../../helper/initDB'
-
+import Blogs from '../../models/blogs'
 initDB()
 console.log("Render")
-export default function handler(req, res) {
-    res.status(200).json(
-        [{
-            "_id":"624c2a601fdbfff983c7773c",
-            "title":"Rubellite Tourmaline Ring",
-            "slug":"rubellite-tourmaline-ring",
-            "newPrice":788,
-            "oldPrice":999,
-            "sku":"SKHDYHD",
-            "category":["Earring","Ring","Cufflink","Bracelet"],
-            "productImage":"https://jewelrydesigns.com/wp-content/uploads/200-01269-Rubellite-Tourmaline-Ring-H-500x500.jpg",
-            "description":"If you're new to jewelry creation, wire wrapping is a fun and relatively simple way to get started. It's also a fantastic opportunity to exhibit your originality and uniqueness because no two pieces are ever the same.",
-            "imageGallery":["https://jewelrydesigns.com/wp-content/uploads/200-01269-Rubellite-Tourmaline-Ring-H-500x500.jpg","https://jewelrydesigns.com/wp-content/uploads/200-01269-Rubellite-Tourmaline-Ring-H-500x500.jpg"]
-        }]
-    )
+export default async function handler(req, res) {
+    const data = await Blogs.find().lean()
+    return res.status(200).send(data)
 }

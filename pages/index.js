b818d12c591aa0c935af320ad4bfd1e9fd73c38a
Sanjay Kumar <sanjaynabha7@gmail.com>
2022-05-25 15:21:23 +0530

    fixed error

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
 import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import baseUrl from '../baseUrl'
-export default function Home({ name, blogData }) {
-  // const [name, setName] = useState("")
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const res = await fetch(`${baseUrl}/api/hello`)
-  //     const data = await res.json()
-  //     setName(data.name)
-  //     console.log(data);
-  //     return data
-  //   }
-  // fetchData();
-  // }, [])
+export default function Home() {
+  const [name, setName] = useState("")
+  const [list, setList] = useState("")
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`${baseUrl}/api/blogs`)
+      const data = await res.json()
+      setList(data)
+      console.log(data);
+      return data
+    }
+  fetchData();
+  }, [])
 
   return (
     <div className={styles.container}>
       My name is {name}. I love Animals.
       <ul>
-        {blogData.map((item, idx) => (
+        {list && list.map((item, idx) => (
           <li key={idx}>{item.title}</li>
         ))}
       </ul>
@@ -27,14 +27,14 @@ export default function Home({ name, blogData }) {
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${baseUrl}/api/hello`)
-  const data = await res.json()
+// export async function getStaticProps() {
+//   const res = await fetch(`${baseUrl}/api/hello`)
+//   const data = await res.json()
 
-  const resBlog = await fetch(`${baseUrl}/api/blogs`)
-  const blogData = await resBlog.json()
+//   const resBlog = await fetch(`${baseUrl}/api/blogs`)
+//   const blogData = await resBlog.json()
 
-  return {
-    props: { name: data.name, blogData },
-  }
-}
+//   return {
+//     props: { name: data.name, blogData },
+//   }
+// }

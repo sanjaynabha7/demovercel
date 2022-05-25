c62ccec5334e32c235be3cdfd9219a1955e2c16b
Sanjay Kumar <sanjaynabha7@gmail.com>
2022-05-25 12:04:32 +0530

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({name}) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch(`${baseUrl}/api/hello`)
   const data = await res.json()
   return {
